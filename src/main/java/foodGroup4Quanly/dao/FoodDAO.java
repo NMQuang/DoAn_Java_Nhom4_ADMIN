424ae04c7af3ee7226ea1d3249dc5294426db3d8
Truong Sinh <ntruongsinh96@gmail.com>
2017-12-24 14:28:50 +0700

    Hien thi ds food, có sử dụng lib phân trang

--- src/main/java/foodGroup4Quanly/dao/FoodDAO.java
+++ src/main/java/foodGroup4Quanly/dao/FoodDAO.java
@@ -1,9 +1,13 @@
 package foodGroup4Quanly.dao;
 
+import java.util.List;
+
 import foodGroup4Quanly.entity.Mon;
 
 
 public interface FoodDAO {
    void save(Mon mon);
    void delete(int id);
+   int getCountFood(boolean active);
+List<Mon> getList(int maxResult, int begin, boolean active);
 }

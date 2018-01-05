5d990d32002404e317c7bc6ba457aba7a36e03ca
Minh Quang <minhquangnguyen1996@gmail.com>
2018-01-05 23:29:52 +0700

    hóa đơn

--- src/main/java/foodGroup4Quanly/dao/HoaDonDao.java
+++ src/main/java/foodGroup4Quanly/dao/HoaDonDao.java
@@ -9,14 +9,20 @@ public interface HoaDonDao {
 	List<Hoadon> getListIn(Date begin, Date end);
 	long getSum(Date begin, Date end);
 	long getCount(Date begin, Date end);
-	
+
 	List<Hoadon> getListIn(Date begin, Date end, int chinhanh);
 	long getSum(Date begin, Date end, int chinhanh);
 	long getCountTypeBill(Date begin, Date end, int chinhanh, String loai);
 	long getCountTypeBill(Date begin, Date end, String loai);
-	
+
 	long getSum(Date begin, Date end, String sdt);
 	long getCount(Date begin, Date end, String sdt);
-	
+
 	Hoadon getTheLastBillByTableStillCooking(int idBan);
+	List<Hoadon> getListHoaDonTaiQuan(int maxResult, int begin);
+	List<Hoadon> getListHoaDonTongDai(int maxResult, int begin);
+	List<Hoadon> getListHoaDonMangVe(int maxResult, int begin);
+	int countTongDai();
+	int countTaiQuan();
+	int countMangVe();
 }
--- src/main/java/foodGroup4Quanly/service/HoadonService.java
+++ src/main/java/foodGroup4Quanly/service/HoadonService.java
@@ -1,12 +1,21 @@
 package foodGroup4Quanly.service;
 
+import java.util.List;
+
 import foodGroup4Quanly.entity.Hoadon;
 
 public interface HoadonService {
-	
+
 	void create(Hoadon hoadon);
 	Hoadon getTheLastBillByTableStillCooking(int idBan);
 	void delete(Hoadon hoadon);
 	void update(Hoadon hoadon);
 	Hoadon getBillById(int id);
+
+	List<Hoadon> getListHoaDonTaiQuan(int maxResult, int begin);
+	List<Hoadon> getListHoaDonTongDai(int maxResult, int begin);
+	List<Hoadon> getListHoaDonMangVe(int maxResult, int begin);
+	int countTongDai();
+	int countMangVe();
+	int countTaiQuan();
 }
